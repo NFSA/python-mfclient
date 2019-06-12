51489206e5ba322c33c3cfda5be5fa2808f5f0e4
Wilson Liu <wliu5@unimelb.edu.au>
2019-06-12 12:00:30 +1000

    Added doc

--- examples/daris_examples.py
+++ examples/daris_examples.py
@@ -4,13 +4,18 @@ MF_HOST = 'mediaflux.researchsoftware.unimelb.edu.au'
 MF_PORT = 443
 MF_TRANSPORT = 'https'
 MF_DOMAIN = 'aaf'
-MF_USER = 'unimelb:_username_'
+MF_USER = 'unimelb:_USERNAME_'
 MF_PASSWORD = '_PASSWORD_'
 
 DARIS_WEB_URL = MF_TRANSPORT.lower() + '://' + MF_HOST.lower() + ':' + str(MF_PORT) + '/daris-web/'
 
 
 def find_scan_by_date(date):
+    """ Prints the DICOM studies scanned on the specified date.
+    :param date: the scan date, in the format of 'dd-MMM-yyyy', e.g. '31-Jan-2017'
+    :type date: str
+    :return:
+    """
     with mfclient.MFConnection(MF_HOST, MF_PORT, MF_TRANSPORT, domain=MF_DOMAIN, user=MF_USER,
                                password=MF_PASSWORD) as cxn:
         # the query string
@@ -30,6 +35,7 @@ def find_scan_by_date(date):
             for cid in cids:
                 print('\tDICOM study: ' + cid)
                 print('\t\tView: ' + DARIS_WEB_URL + '/#view_' + cid)
+        return cids
 
 
 if __name__ == '__main__':
